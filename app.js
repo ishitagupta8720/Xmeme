b83757ba7484e603a67e5b6650a444d22fe81f8e
ishitagupta8720 <45250159+ishitagupta8720@users.noreply.github.com>
2021-02-10 01:06:26 +0530

    changed port

--- app.js
+++ app.js
@@ -181,7 +181,8 @@ app.use((req, res) =>{
     res.status(404).send('404 Not Found');
 })
 
-const port = process.env.PORT || 8081;
-app.listen(port, () => {
-    console.log("APP IS LISTENING ON PORT 8081!")
-})
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
